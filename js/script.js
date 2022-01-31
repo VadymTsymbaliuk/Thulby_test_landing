ec84724a7d3b8635da97a4e0245b697b44becac0
Vadym Tcimbaliuk <vadymonua@gmail.com>
2022-01-31 19:13:34 +0200

    fix animation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-const btnSubscribe = document.querySelector('.subscribe')
+const form = document.querySelector('.form-container')
 const subscribeForm = document.querySelector('.form-container')
 const loader = document.querySelector('.loader-bg')
 
-btnSubscribe.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
      e.preventDefault()
     subscribeForm.classList.toggle('active')
     loader.classList.toggle('active')
 
+    document.querySelector('#firstName').value = ''
+    document.querySelector('#lastName').value = ''
+    document.querySelector('#email').value = ''
     setTimeout(()=>{
         subscribeForm.classList.toggle('active')
         loader.classList.toggle('active')
